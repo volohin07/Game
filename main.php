7c4e665100731dc190cda4f37d4d3ebc3b67f339
volohin07 <volohin07@gmail.com>
2020-02-15 12:28:47 +0200

    new update

--- main.php
+++ main.php
@@ -12,12 +12,14 @@ $col_1 = $columns->addColumn(3);
 $col_2 = $columns->addColumn(10);
 $col_3 = $columns->addColumn(3);
 
- $clicker = $col_2->add(["Button",$_SESSION["i"],"green fluid big"]);
- $clicker->on('click',function ($m) {
-   $_SESSION["i"] = $_SESSION["i"] + 1;
-   return $m;
- });
+ $val = $col_2->add(['FormField/line', '1']);
+
+ $clicker = $col_2->add(["Button","Click!","green fluid big"]);
+ $clicker->js('click',new \atk4\ui\jsReload($val,['val' => $val->jsInput()->val(new \atk4\ui\jsExpression('parseInt([])+1', [$val->jsInput()->val()]), $val->jsInput()->focus())]));
 
 $save = $col_2->add(["Button","Save","blue big"]);
 $exit = $app->add(['Button',"Exit","red"]);
 $exit->link(["exit"]);
+$x2 = $col_3->add(["Button","click x3","inverted orange button","medium ui button","icon"=>"power off"]);
+$pus = $col_3->add(["Button","click x5","inverted primary button","medium ui button","icon"=>"power off"]);
+$x10 = $col_3->add(["Button","+10000 points","inverted yellow button","medium ui button","icon"=>"power off"]);
